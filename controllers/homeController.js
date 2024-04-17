a81ede6f55c4f0f633b7df5f6777a5f2a8b7c181
정민김 <74294488+sigjm@users.noreply.github.com>
2024-04-17 02:28:17 +0000

    임시

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,4 @@
 // homeController.js
-"use strict";
 
 /**
  * Listing 12.5 (p. 178)
@@ -14,3 +13,35 @@
  * [노트] 뷰 안에서는 offeredCourses라는 변수명을 통해 배열에 접근할 수 있다.
  * 홈 컨트롤러 내에서 이 배열은 courses라는 이름으로 다룬다.
  */
+// homeController.js
+"use strict";
+
+const { name } = require("ejs");
+
+/**
+ * Listing 10.2 (p. 159)
+ * 컨트롤러 동작에 의한 뷰 렌더링
+ */
+exports.respondWithName1; // @TODO: 사용자 정의 EJS 뷰를 사용한 응답
+
+/**
+ * Listing 10.3 (p. 160)
+ * 뷰로의 라우트 매개변수 전달
+ */
+exports.respondWithName = (req, res) => {
+    let paramsName = req.params.myName;
+    res.render('index', {
+        name: paramsName,
+        people: {
+            first: {
+                name : "Aaron",
+                city : "seoul"
+            },
+            second: {
+                name: "jj",
+                city: "seoul"
+            }
+        }
+    });    
+}; // @TODO: 요청 매개변수로 지역 변수 할당. 그다음 렌더링된 뷰로 지역 변수 전달.
+
--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 // main.js
 // Capstone 2: Express
 "use strict";
+const port = 3001,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
 
+    app = express();
 // 앱 설정
 
 /**
  * Listing 12.7 (p. 179)
  * ejs 레이아웃 렌더링
  */
-
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs");
 
 /**
  * Listing 12.4 (p. 177)
@@ -20,6 +25,8 @@
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
+
+
 
 
 /**
