36ff3a1a18206174182741a9a500751b5b5b8f98
정민김 <74294488+sigjm@users.noreply.github.com>
2024-04-20 08:55:02 +0000

    Pending changes exported from your codespace

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
 // main.js
 // Capstone 2: Express
 "use strict";
-const port = 3001,
-    express = require('express'),
+const port = 3000, 
+    express = require("express"),
     layouts = require('express-ejs-layouts'),
+    
+    homeController = require("./controllers/homeController"),
+    
+    app =express();
+    
+app.set("port", process.env.PORT || port);
 
-    app = express();
+app.set("view engine", "ejs");
+app.use(layouts);
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Confetti Cusine!")
+});    
 // 앱 설정
 
 /**
  * Listing 12.7 (p. 179)
  * ejs 레이아웃 렌더링
  */
-app.set("port", process.env.PORT || port);
-app.set("view engine", "ejs");
+
+
+
 
 /**
  * Listing 12.4 (p. 177)
  * body-parser의 추가
  */
-
-
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
 /**
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
-
-
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/courses", homeController.postedSignUpForm);
 
 
 /**
@@ -36,3 +55,6 @@ app.set("view engine", "ejs");
 
 
 // 3000번 포트로 리스닝 설정
+app.listen(app.get("port"), () => {
+    console.log(`Server at http://localhost:${app.get("port")}`);
+});
